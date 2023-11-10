bdbb7ffc9e7d39738dd16f88f8b4228339767f39
Jessica Salvino <150205230+heysalvino@users.noreply.github.com>
2023-11-10 15:43:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan'); // Middleware for logging
+const helmet = require('helmet'); // Middleware for securing HTTP headers
+
 const app = express();
-const port =3000;
-var path = require('path');
+const port = process.env.PORT || 3000;
+
+// Middleware
 app.use(express.static('views'));
+app.use(express.json()); // Parse JSON requests
+app.use(morgan('dev')); // Log requests to the console
+app.use(helmet()); // Add security headers
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function(req, res) {
+// Routes
+app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Sample API endpoint
+app.get('/api/sample', (req, res) => {
+    const data = { message: 'This is a sample API endpoint.' };
+    res.json(data);
+});
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
